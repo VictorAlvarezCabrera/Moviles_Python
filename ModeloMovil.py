d5eaa50a576bdcba80c1dfbb82be6b1b70209e1c
Victor <alvarezcabreravictor@gmail.com>
2025-04-03 10:23:43 +0200

    He añadido para __eq__ para poder igualar moviles y __str__ para poder implimir las caracteristicas del objeto

--- ModeloMovil.py
+++ ModeloMovil.py
@@ -27,3 +27,26 @@ class ModeloMovil:
             self.ram = 0
         else:
             self.ram = ram
+        
+        if precio < 0:
+            self.precio = 0.0
+        else:
+            self.precio = float(round(precio, 2))
+
+    def __eq__(self, other):
+        if isinstance(other, self):
+            return self.modelo == other.modelo and self.marca == other.marca
+        return False
+
+    def __str__(self):
+        return(
+            "marca: ", self.marca, "\n"
+            "modelo: ", self.modelo, "\n"
+            "año de lanzamiento: ", self.anio_lanzamiento, "\n"
+            "sistema operativo: ", self.sistema_operativo, "\n"
+            "bateria: ", self.bateria, "\n"
+            "almacenamiento: ", self.almacenamiento, "\n"
+            "memoria ram: ", self.ram, "\n"
+            "precio: ", self.precio, "€\n"
+
+        )
